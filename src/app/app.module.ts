6474c793658661ae8e9f1b1942ede15a1c9d2fa4
Behruz <mrbekuwl@gmail.com>
2018-03-24 18:06:55 +0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { routing } from './app.routing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    CheckoutComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+import { MainComponent } from './main/main.component';
+import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes : Routes  = [
+    {
+        path:'',
+        component: AppComponent,
+        children: [
+            {
+                path:'',
+                component: MainComponent,
+                children: [
+                    {
+                        path: '',
+                        component: ContentComponent
+                    }
+                ]
+            }
+        ]
+    }, 
+
+]
+
+export const routing = RouterModule.forRoot(routes);
