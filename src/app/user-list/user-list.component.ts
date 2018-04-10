ed5c52520102750d00b742f777a4dd6940ba8a39
mrbeku <mrbekuwl@gmail.com>
2018-04-10 14:57:36 +0500

    user-list component modified

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+declare interface TableData {
+  headerRow : string[];
+  dataRows: string[][];
+}
+
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
@@ -7,9 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
+  public TableData : TableData;
+
   constructor() { }
 
   ngOnInit() {
+    this.TableData = {
+      headerRow: ['ID', 'Name', 'Username', 'Email', 'Address'],
+      dataRows: [
+        ['1', '', '', '', '', ''],
+        ['2', '', '', '', '', '']
+      ]
+    };
+
+
   }
 
 }
