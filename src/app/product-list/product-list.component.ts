1ac76871f5cae3ae3c7a5cde0e8e6d2f21161ffd
mrbeku <mrbekuwl@gmail.com>
2018-04-10 15:04:39 +0500

    product-list component modfied

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare interface TableData {
+  headerRow : string[];
+  dataRows: string[][];
+}
+
 @Component({
   selector: 'admin-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  
+  public TableData : TableData;
 
   constructor() { }
 
   ngOnInit() {
+    this.TableData = {
+      headerRow: ['ID', 'Creator', 'Title', 'Price', 'Date', 'Subject', 'Publisher', 'Type', 'Format', 'Identifier', 'Source', 'Language', 'Relation', 'Coverage', 'Rights'],
+      dataRows: [
+        ['1', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['2', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+      ]
+    };
   }
 
 }
