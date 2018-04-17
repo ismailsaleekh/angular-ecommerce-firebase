eaf3ccb32f32c14053a271b610a8e879f36bf6f7
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-17 19:54:54 +0500

    feat: product-list in admin panel connected to database

--- src/app/components/admin/product-list/product-list.component.ts
+++ src/app/components/admin/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { DataService } from './../../../services/data.service';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -13,11 +14,13 @@ declare interface TableData {
 })
 export class ProductListComponent implements OnInit {
   
+  public productsList: object[]: []
   public TableData : TableData;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.productsList = await this.dataService.fetchProducts()
     this.TableData = {
       headerRow: ['ID', 'Creator', 'Title', 'Price', 'Date', 'Subject', 'Publisher', 'Type', 'Format', 'Identifier', 'Source', 'Language', 'Relation', 'Coverage', 'Rights'],
       dataRows: [
