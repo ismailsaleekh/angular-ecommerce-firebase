511fd1c696587ac67e04813b20483438e9f9e0bb
bekh <mrbekuwl@gmail.com>
2018-04-18 16:19:39 +0500

    style: carousel images size changed, added new images for carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [
+    { provide: CarouselConfig, useValue: { interval: 3000, noPause: true, showIndicators: false } }
+  ]
 })
 export class CarouselComponent implements OnInit {
 
