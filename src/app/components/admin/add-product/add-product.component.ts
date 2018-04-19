383f29073e4ac18724428c510ccbee7eccafdfb4
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-19 16:16:40 +0500

    feat: add-product component now used for editing also

--- src/app/components/admin/add-product/add-product.component.ts
+++ src/app/components/admin/add-product/add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as feather  from 'feather-icons';
+import * as feather from 'feather-icons';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-add-product',
@@ -8,9 +10,37 @@ import * as feather  from 'feather-icons';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  public product: any = {}
+  public cover: File
+  public isEdit: boolean = false
+  
+  constructor(private acRt: ActivatedRoute,
+              private dataService: DataService
+  ) { }
 
   ngOnInit() {
     feather.replace();
+    this.acRt.queryParams.filter(param => param.isEdit).subscribe((data: any) => {
+      this.isEdit = data.isEdit    
+    })
+    if (this.isEdit) {
+      this.product = this.dataService.selectedProduct
+    } else {
+      this.product = {}
+    }
+  }
+
+  public submitForm() {
+    if (this.isEdit) {
+      this.dataService.updateProduct(this.product.key, this.product)
+    } else {
+      this.dataService.addProduct(this.product)
+    }
+  }
+
+  public onFileChange(file) {
+    if (file) {
+      this.product.image = file
+    }
   }
 }
