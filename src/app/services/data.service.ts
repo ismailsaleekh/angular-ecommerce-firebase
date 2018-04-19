f165e0af3455f98e3bce82e373b9de5d9d811c88
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-19 16:48:52 +0500

    feat: added function for managing products

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,25 +1,77 @@
 import { Injectable } from "@angular/core";
-import { AngularFireDatabase } from "angularfire2/database";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+import { AngularFireStorage } from "angularfire2/storage";
 
 
 @Injectable()
 
 export class DataService {
 
-    private products: object[] = []
+  private products: object[] = []
+  private genres: object[] = []
+  private authors: object[] = []
+  private productsRef: AngularFireList<any>
+  
+  public selectedProduct: object = {}
 
-    constructor(private database: AngularFireDatabase) { }
+  constructor(private database: AngularFireDatabase,
+              private storage: AngularFireStorage
+  ) { 
+    this.productsRef = this.database.list('products')    
+  }
 
-    public async fetchProducts(): Promise<any> {
-      await this.database.list('/products/').snapshotChanges()
-        .subscribe((data: any): void => {
-          data.forEach(element => {
-            const product = element.payload.val()
-              product.key = element.key
-              this.products.push(product)
-            });
-        })
-      return this.products
-    }
+  public async fetchProducts(): Promise<any> {
+    await this.database.list('/products/').snapshotChanges()
+      .subscribe((data: any): void => {
+        data.forEach(element => {
+          const product = element.payload.val()
+          product.key = element.key
+          this.products.push(product)
+        });
+      })
+    return this.products
+  }
 
+  public async fetchGenres(): Promise<any> {
+    await this.database.list('/genres/').snapshotChanges()
+      .subscribe((data: any): void => {
+        data.forEach(element => {
+          const genre = element.payload.val()
+          genre.key = element.key
+          this.genres.push(genre)
+        });
+      })
+
+    return this.genres
+  }
+
+  public async fetchAuthors(): Promise<any> {
+    await this.database.list('/authors/').snapshotChanges()
+      .subscribe((data:any): void => {
+        data.forEach(element => {
+          const author = element.payload.val()
+          author.key = element.key
+          this.authors.push(author)
+        });
+      })
+    
+    return this.authors
+  }
+
+  public async updateProduct(key: string, product: object) {
+    await this.productsRef.update(key, product)
+  }
+
+  public async deleteProduct(key: string) {
+    await this.productsRef.remove(key)
+  }
+
+  public addProduct(product) {
+    this.storage.upload(`${product.title}_Cover`, product.image).then((data: any) => {
+      product.coverUrl = data.metadata.downloadURLs[0]
+      console.log(product)
+    }).then(() => {
+      this.database.list('/products/').push(product)
+    })
+  }
 }
