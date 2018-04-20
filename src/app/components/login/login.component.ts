c17ee4391025e6ce714878010e06b744cd6318b5
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-20 16:43:43 +0500

    chore: added login methods and register modal

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private modalRef: BsModalRef
+  public user: any = {}
+  public registringUser: any = {}
+  public sent: boolean = false
+
+  public message: string = ''
+
+  constructor(private userService: UserService,
+              private modal: BsModalService
+  ) { }
 
   ngOnInit() {
+    this.userService.loginFailed.subscribe(data => {
+      this.message = data
+    })
   }
 
+  public signGoogle() {
+    this.userService.signInWithGoogle()
+  }
+
+  public signFacebook() {
+    this.userService.signWithFacebook()
+  }
+
+  public signTwitter() {
+    this.userService.signWithTwitter()
+  }
+
+  public signWithEmail() {
+    this.userService.signInWithEmail(this.user.email, this.user.password)
+  }
+
+  public showModal(template) {
+    this.modalRef = this.modal.show(template)
+  }
+
+  public register() {
+    this.userService.registerUserWithEmail(this.registringUser.email, this.registringUser.password)
+    this.sent = true
+  }
 }
