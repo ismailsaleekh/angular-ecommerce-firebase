ca55f8281180b4498ef44af83e0a62a7af22c40b
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-20 16:43:09 +0500

    fix: not changing label on user log in

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isAuth: boolean = false
+
+  constructor(private usersService: UserService) { }
 
   ngOnInit() {
+    this.usersService.isAuth.subscribe(data => {
+      this.isAuth = data
+    })
   }
 
+  logout() {
+    this.usersService.logout()
+  }
 }
