041812d16b190514bc826697396ebfe5da4d8ea9
bekh <mrbekuwl@gmail.com>
2018-04-21 23:11:44 +0500

    style: 2 tables added with feather icons

--- src/app/components/user-account/user-account.component.ts
+++ src/app/components/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as feather from 'feather-icons'
 
 @Component({
   selector: 'app-user-account',
@@ -10,6 +11,7 @@ export class UserAccountComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    feather.replace();
   }
 
 }
